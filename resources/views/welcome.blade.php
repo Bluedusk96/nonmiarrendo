ad84cf3f9b58f7ad36cf5f23a52f37c16db1080b
Bluedusk96 <69deep96@gmail.com>
2016-03-23 23:09:17 +0100

    ultimi ritocchi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Anyar-HTML Bootstrap theme</title>
+    <title>Broccolo Bill Family Restaurant</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
